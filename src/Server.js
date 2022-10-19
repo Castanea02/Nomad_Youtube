a86b1109ee8aaaa0f531e1061d154a1dacc8ada9
CA-STANEA <ahrhk2000@gmail.com>
2022-10-19 13:09:21 +0900

    Learn MiddleWare

--- src/Server.js
+++ src/Server.js
@@ -1,24 +1,25 @@
 import express from "express";
-
 const PORT = 8080;
 
 //express start
 const app = express();
-
-//Request Function
-const Req = () => 
-    console.log("Somebody is Request /");
-
-//Request
-app.get("/", Req);
-
-
-
-
-
-
+//Server ListenLogging
 const handleListening = () => 
     console.log(`Server listening on port ahrhk2000.direct.quickconnect.to:${PORT}`);
+
+//Middleware Function
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+//Request, Response Function
+const RequestHome = (req, res, next) => {
+    return res.send("<h1>Send Massage</h1>");
+};
+
+app.use(logger);
+app.get("/", RequestHome);
 
 //Server Listen
 app.listen(PORT, handleListening);
