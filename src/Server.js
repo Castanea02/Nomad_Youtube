93a27a4934ec439d13378ad4a8ac895e5902ccf6
CA-STANEA <ahrhk2000@gmail.com>
2022-10-19 14:47:55 +0900

    Making Routers

--- src/Server.js
+++ src/Server.js
@@ -1,25 +1,30 @@
 import express from "express";
+import morgan from "morgan";
 const PORT = 8080;
 
 //express start
 const app = express();
+const logger = morgan("dev");
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("HOME");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
 //Server ListenLogging
 const handleListening = () => 
     console.log(`Server listening on port ahrhk2000.direct.quickconnect.to:${PORT}`);
-
-//Middleware Function
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-
-//Request, Response Function
-const RequestHome = (req, res, next) => {
-    return res.send("<h1>Send Massage</h1>");
-};
-
-app.use(logger);
-app.get("/", RequestHome);
-
 //Server Listen
 app.listen(PORT, handleListening);
