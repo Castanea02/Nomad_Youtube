575ad0dd010e7623fbbe4d4d29888c1089742dfc
CA-STANEA <ahrhk2000@gmail.com>
2022-11-05 18:07:21 +0900

    Learn Conditionals & Iteration in Pug

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,18 +1,13 @@
 //VideoController export
 //요청시 반환
-export const trending = (req, res) => res.render("home");
-
+export const trending = (req, res) => {
+    const videos = [];
+    res.render("home", {pageTitle : "Home", videos});
+};
 export const see = (req, res) => res.render("watch");
-
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit Video");
-}
-
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
-
 export const upload = (req, res) => res.send("Upload Video");
-
 export const deleteVideo = (req, res) => {
     console.log(req.params);
     return res.send("Delete Video");
