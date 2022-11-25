23e3fc23acad2c6a8a63b0d8f819e8134dd1dae3
castanea02 <ahrhk2000@gmail.com>
2022-11-25 15:58:34 +0900

    Create Search Video

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -4,7 +4,7 @@ const requestIp = require('request-ip');
 
 export const home = async(req, res) => { //메인페이지 로드 home.pug
     console.log("Client IP : " + requestIp.getClientIp(req)); //IP Logging
-    const videos = await Video.find({}); //await, async (promise) 비디오 불러오기
+    const videos = await Video.find({}).sort({createdAt:"asc"}); //await, async (promise) 비디오 불러오기
 
     return res.render("home", {pageTitle:"Home", videos});
 };
@@ -70,4 +70,17 @@ export const deleteVideo = async(req, res) =>{
     await Video.findByIdAndDelete(id);
 
     return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+    const {keyword}=req.query; //Get Param
+    let videos = [];
+    if(keyword){
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i"),//정규 표현식 keyword를 포함한
+            },
+        });
+    }
+    return res.render("search", {pageTitle:"Search", videos});
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,13 +1,13 @@
 //글로벌 라우터
 import express from "express";
 import {join, login} from "../controller/userController";
-import {home} from "../controller/videoController";
+import {home, search} from "../controller/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-
+globalRouter.get("/search", search);
 
 export default globalRouter;
